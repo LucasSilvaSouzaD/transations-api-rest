1e0ce3e76b0433f09abaa792b474f6ee2d126536
LucasSilvaSouzaD <lucassilvadonato23@gmail.com>
2024-07-29 19:03:00 -0300

    up project learning

---
+++ src/server.ts
@@ -0,0 +1,19 @@
+import fastify from 'fastify'
+import { env } from './env'
+import { transactionsRoutes } from './routes/transactionsRoutes'
+import cookie from '@fastify/cookie'
+
+const app = fastify()
+
+app.register(cookie)
+app.register(transactionsRoutes, {
+  prefix: 'transactions',
+})
+
+app
+  .listen({
+    port: env.PORT,
+  })
+  .then(() => {
+    console.log('Http Server Running!')
+  })
